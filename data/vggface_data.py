50ee1ff00a88ae51a101dbfca38d8a82f48a23c1
Šimon Strýček <xstryc06@stud.fit.vutbr.cz>
2023-11-08 11:02:04 +0100

    Add support for VGG-Face2 dataset

---
+++ data/vggface_data.py
@@ -0,0 +1,50 @@
+import numpy
+from torch.utils.data import Dataset
+import torch
+import cv2
+import pandas as pd
+import numpy as np
+from os.path import join
+from os import listdir
+from os.path import isfile
+from os.path import isdir
+import tarfile
+from zipfile import ZipFile
+from PIL import Image
+
+
+class VGGFaceDataset(Dataset):
+    def __init__(self, img_dir, file_list_path, transform=None, device=None):
+        self.img_root = img_dir
+
+        with open(file_list_path) as f:
+            self.img = [line for line in f.read().splitlines() if isfile(join(img_dir, line))]
+
+        self.ann = [int(img.split('/')[-2].removeprefix("n")) for img in self.img]
+        self.transform = transform
+        self.device = device
+
+    def __len__(self):
+        return len(self.img)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_path = join(self.img_root, self.img[idx])
+        image = cv2.imread(img_path) / 255.0
+        ann = torch.tensor(self.ann[idx])
+
+        if self.transform:
+            image = self.transform(image)#["image"]
+        #image = torch.tensor(image)
+
+        if self.device:
+            image = image.to(self.device)
+            ann = ann.to(self.device)
+
+        sample = {'image': image, 'entity': ann}
+        return sample
+
+    def num_of_classes(self):
+        return max(self.ann) + 1
