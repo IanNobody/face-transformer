b78aad654e11c65b2913296319cfdea48ccb5163
Šimon Strýček <xstryc06@stud.fit.vutbr.cz>
2024-02-06 15:50:47 +0100

    Refactor training

--- training/train.py
+++ training/train.py
@@ -37,42 +37,16 @@ def train(model, dataloader, model_optimizer, loss_optimizer, model_scheduler, l
             running_loss += current_loss
 
             if bidx % config.checkpoint_freq == config.checkpoint_freq - 1:
-                checkpoint(model,
-                           model_optimizer,
-                           loss_optimizer,
-                           dataloader,
-                           criterion,
-                           top_checkpoints,
-                           config,
-                           current_loss,
-                           epoch,
-                           bidx)
+                checkpoint(model, model_optimizer, loss_optimizer, dataloader, criterion, top_checkpoints,
+                           config, current_loss, epoch, bidx)
 
-        checkpoint(model,
-                   model_optimizer,
-                   loss_optimizer,
-                   dataloader,
-                   criterion,
-                   top_checkpoints,
-                   config,
-                   running_loss / len(dataloader),
-                   epoch,
-                   0,
-                   permanent=True)
+        checkpoint(model, model_optimizer, loss_optimizer, dataloader, criterion, top_checkpoints,
+                   config, running_loss / len(dataloader), epoch, 0, permanent=True)
         print("Epoch ", epoch, " completed with loss: ", running_loss / len(dataloader))
 
 
-def checkpoint(model,
-               model_optimizer,
-               criterion_optimizer,
-               dataloader,
-               criterion,
-               top_checkpoints,
-               config,
-               loss,
-               epoch,
-               bidx,
-               permanent=False):
+def checkpoint(model, model_optimizer, criterion_optimizer, dataloader, criterion, top_checkpoints,
+               config, loss, epoch, bidx, permanent=False):
     new_checkpoint = "checkpoint-" + str(epoch) + ".pth" if permanent else get_checkpoint_filename(epoch, bidx)
 
     if not permanent:
@@ -82,7 +56,8 @@ def checkpoint(model,
         top_checkpoints.append(new_checkpoint)
 
     save_checkpoint(model, model_optimizer, criterion_optimizer, dataloader, criterion, config, new_checkpoint)
-    print("Epoch: ", epoch, "| Batch: ", bidx, "| Loss: ", "{:.2f}".format(loss), "| Checkpoint file: ", new_checkpoint)
+    print("Epoch: ", epoch, "| Batch: ", bidx, "| Loss: ", "{:.2f}".format(loss), " | Model lr: ",
+          model_optimizer.param_groups[0]["lr"], " | Loss lr: ", criterion_optimizer.param_groups[0]["lr"])
 
 
 def get_checkpoint_filename(epoch, bidx):
