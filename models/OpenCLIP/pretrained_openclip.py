000c688d9c777673e3d4a13f5a11839e7be90021
Šimon Strýček <xstryc06@stud.fit.vutbr.cz>
2024-02-11 19:08:53 +0100

    Extract only visual OpenCLIP part

--- models/OpenCLIP/pretrained_openclip.py
+++ models/OpenCLIP/pretrained_openclip.py
@@ -1,16 +1,23 @@
 import torch.nn as nn
 from transformers import CLIPProcessor, CLIPModel
 
+
 class OpenCLIPWrapper(nn.Module):
     def __init__(self, device):
         super(OpenCLIPWrapper, self).__init__()
         self.device = device
-        self.clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-        self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+        self.clip_vision = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").vision_model
+        self.fc = nn.Linear(768, 512)
+        # self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-    def forward(self, imgs, txts):
-        if len(imgs) != len(txts):
-            raise ValueError("Number of images and text descriptions must be the same.")
+    def forward(self, x):
+        y = self.clip_vision(x).pooler_output
+        y = self.fc(y)
+        return y
 
-        preprocessed_data = self.processor(text=txts, images=imgs, return_tensors="pt", padding=True).to(self.device)
-        return self.clip_model(**preprocessed_data)
+    # def forward(self, imgs, txts):
+    #     if len(imgs) != len(txts):
+    #         raise ValueError("Number of images and text descriptions must be the same.")
+    #
+    #     preprocessed_data = self.processor(text=txts, images=imgs, return_tensors="pt", padding=True).to(self.device)
+    #     return self.clip_model(**preprocessed_data)
