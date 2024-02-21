8c67ad48e9ef64418d7785536c281a7a4f70e8d0
Šimon Strýček <xstryc06@stud.fit.vutbr.cz>
2024-02-21 17:12:56 +0100

    Implement additional metadata loading

--- data/vggface_data.py
+++ data/vggface_data.py
@@ -1,26 +1,33 @@
-import numpy
 from torch.utils.data import Dataset
 import torch
 import cv2
-import pandas as pd
-import numpy as np
 from os.path import join
-from os import listdir
 from os.path import isfile
-from os.path import isdir
-import tarfile
-from zipfile import ZipFile
-from PIL import Image
-
 
 class VGGFaceDataset(Dataset):
-    def __init__(self, img_dir, file_list_path, transform=None, augmentation=None, device=None):
+    def __init__(self, img_dir, meta_dir, transform=None, augmentation=None, device=None):
         self.img_root = img_dir
 
-        with open(file_list_path) as f:
+        with open(join(meta_dir, "train_list_aligned.txt")) as f:
             self.img = [line for line in f.read().splitlines() if isfile(join(img_dir, line))]
 
-        self.ann = [int(img.split('/')[-2].removeprefix("n")) for img in self.img]
+        self.cls = [img.split('/')[-2] for img in self.img]
+        self.cls_dict = {cls: idx for idx, cls in enumerate(set(self.cls))}
+
+        with open(join(meta_dir, "identity_meta.csv")) as f:
+            lines = f.read().splitlines()[1:]
+            self.gender = {line.split(", ")[0]: (1 if line.split(", ")[-1] == 'm' else 0) for line in lines}
+
+        self.hair = {cls: 0 for cls in self.img}
+
+        for hair_id, hair_meta_file in enumerate(["02-Black_Hair.txt", "03-Brown_Hair.txt", "04-Gray_Hair.txt", "05-Blond_Hair.txt"]):
+            with open(join(meta_dir, hair_meta_file)) as f:
+                lines = f.read().splitlines()[1:]
+                for line in lines:
+                    meta = line.split()
+                    if meta[1] == "1":
+                        self.hair[meta[0]] = hair_id + 1
+
         self.transform = transform
         self.augmentation = augmentation
         self.device = device
@@ -32,9 +39,14 @@ class VGGFaceDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        img_path = join(self.img_root, self.img[idx])
-        image = cv2.imread(img_path)# / 255.0
-        ann = torch.tensor(self.ann[idx])
+        img_id = self.img[idx]
+        img_path = join(self.img_root, img_id)
+        image = cv2.imread(img_path)
+
+        cls_name = self.cls[idx]
+        ann = torch.tensor(self.cls_dict[cls_name])
+        gender = torch.tensor(self.gender[cls_name])
+        hair = torch.tensor(self.hair[img_id])
 
         if self.augmentation:
             image = self.augmentation(image=image)["image"]
@@ -42,11 +54,7 @@ class VGGFaceDataset(Dataset):
         if self.transform:
             image = self.transform(image)
 
-        # if self.device:
-            # image = image.to(self.device)
-            # ann = ann.to(self.device)
-
-        sample = {'image': image, 'entity': ann}
+        sample = {'image': image, 'class': ann, 'gender': gender, 'hair': hair}
         return sample
 
     def num_of_classes(self):
