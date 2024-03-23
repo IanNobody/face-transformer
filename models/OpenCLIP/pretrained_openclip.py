7def4d21e72b02907745c063f16889d7abceae2f
Šimon Strýček <xstryc06@stud.fit.vutbr.cz>
2024-03-23 13:53:20 +0100

    Add classification task to OpenCLIP

--- models/OpenCLIP/pretrained_openclip.py
+++ models/OpenCLIP/pretrained_openclip.py
@@ -3,17 +3,19 @@ from transformers import CLIPProcessor, CLIPModel
 
 
 class OpenCLIPWrapper(nn.Module):
-    def __init__(self, device=None):
+    def __init__(self, num_classes, device=None):
         super(OpenCLIPWrapper, self).__init__()
         self.device = device
         self.clip_vision = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").vision_model
-        self.fc = nn.Linear(768, 512)
+        self.embed_fc = nn.Linear(768, 512)
+        self.class_fc = nn.Linear(768, num_classes)
         # self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
     def forward(self, x):
         y = self.clip_vision(x).pooler_output
-        y = self.fc(y)
-        return y
+        embed = self.embed_fc(y)
+        cls = self.class_fc(y)
+        return {"embedding": embed, "class": cls}
 
     # def forward(self, imgs, txts):
     #     if len(imgs) != len(txts):
