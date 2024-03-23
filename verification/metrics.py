fa80e610f23b4b248a61b2613a9f2af7fb76fa07
Šimon Strýček <xstryc06@stud.fit.vutbr.cz>
2024-03-23 13:54:05 +0100

    Add manual metrics implementation

--- verification/metrics.py
+++ verification/metrics.py
@@ -7,6 +7,7 @@ import os
 from checkpointing.checkpoint import load_weights
 from eer import eer
 import numpy as np
+import bisect
 
 
 class Metrics:
@@ -44,8 +45,8 @@ class Metrics:
                     embedding1 = self.model(img1)["embedding"].cpu()
                     embedding2 = self.model(img2)["embedding"].cpu()
                 else:
-                    embedding1 = self.model(img1).cpu()
-                    embedding2 = self.model(img2).cpu()
+                    embedding1 = self.model(img1)["embedding"].cpu()
+                    embedding2 = self.model(img2)["embedding"].cpu()
 
                 similarity = [self._similarity(em1, em2) for em1, em2 in zip(embedding1, embedding2)]
                 self.similarities.extend(similarity)
@@ -82,11 +83,12 @@ class Metrics:
                             self._run_stats()
 
                             fpr, tpr, thresholds, roc_auc = self._generate_roc()
+                            self._print_all_tar_at_far(fpr, tpr)
                             best_threshold = self._get_best_threshold(fpr, tpr, thresholds, 0.0001)
 
                             filepath = os.path.join(output_dir, "roc-" + str(checkpoint_number) + ".png")
                             if filepath:
-                                self._save_roc(fpr, tpr, roc_auc, filepath)
+                                self._save_roc(fpr, tpr, thresholds, roc_auc, filepath)
 
                             stats = self._threshold_based_statistics(best_threshold)
                             self._append_stats(stats, best_threshold, roc_auc)
@@ -100,15 +102,20 @@ class Metrics:
             #     print("Error occurred while processing files. \nError description: ", e)
             #     exit(-1)
 
-    def test_and_print(self, output_dir):
+    def test_and_print(self, output_dir, index):
         self._run_stats()
         fpr, tpr, thresholds, roc_auc = self._generate_roc()
+        self._print_all_tar_at_far(fpr, tpr)
         best_threshold = self._get_best_threshold(fpr, tpr, thresholds, 0.0001)
-        filepath = os.path.join(output_dir, "roc.png")
-        self._save_roc(fpr, tpr, roc_auc, filepath)
+        filepath = os.path.join(output_dir, "roc-" + str(index) + ".png")
+        self._save_roc(fpr, tpr, thresholds, roc_auc, filepath)
+        self._save_tpr_fpr(tpr, fpr, os.path.join(output_dir, "tpr_fpr-" + str(index) + ".png"))
         stats = self._threshold_based_statistics(best_threshold)
         self._append_stats(stats, best_threshold, roc_auc)
+        #self._compute_print(self.similarities, self.labels)
         self._print_stats()
+        self.plot_accuracy_progress(self.similarities, self.labels, index)
+        self.plot_tar_progress(self.similarities, self.labels, index)
 
     def _append_stats(self, stats, threshold, roc_auc):
         self.stats["threshold"].append(threshold)
@@ -122,29 +129,10 @@ class Metrics:
         self.stats["recall"].append(stats["true_positive"] / (stats["true_positive"] + stats["false_negative"]))
 
     def _print_stats(self):
-        print("-------- Thresholds --------")
-        print(self.stats["threshold"])
-        print("------------ AUC ------------")
-        print(self.stats["roc_auc"])
-        print("------------ F1 ------------")
-        print(self.stats["f1"])
-        print("--------- Accuracy ---------")
-        print(self.stats["accuracy"])
-        print("------------ EER ------------")
-        print(self.stats["eer"])
-        print("------------ TPR ------------")
-        print(self.stats["tpr"])
-        print("------------ FPR ------------")
-        print(self.stats["fpr"])
-        print("--------- Precision ---------")
-        print(self.stats["precision"])
-        print("----------- Recall -----------")
-        print(self.stats["recall"])
-        print("------------ BEST ------------")
-        print("Max F1: ", max(self.stats["f1"]))
-        print("Max Accuracy: ", max(self.stats["accuracy"]))
-        print("Min EER: ", min(self.stats["eer"]))
-        print("------------------------------")
+        print("Threshold:", self.stats["threshold"])
+        print("F1: ", self.stats["f1"])
+        print("Accuracy: ", self.stats["accuracy"])
+        print("EER: ", self.stats["eer"])
 
     def _generate_roc(self):
         fpr, tpr, thresholds = roc_curve(self.labels, self.similarities)
@@ -154,23 +142,179 @@ class Metrics:
     def _get_eer(self):
         return eer(self.similarities, self.labels)
 
+    def _print_tar_at_far(self, fpr, tpr, far_rate):
+        index = bisect.bisect_left(fpr, far_rate) - 1
+        length = fpr[index + 1] - fpr[index]
+        diff = far_rate - fpr[index]
+        interpol_coeff = diff / length
+        interpol = tpr[index] + interpol_coeff * (tpr[index + 1] - tpr[index])
+        print("> TAR(%)@FAR=" + str(far_rate) + ":", tpr[index], " (interpolated: " + str(interpol) + ")")
+
+    def compute_tar_at_far(genuine_scores, impostor_scores, target_FAR):
+        # Combine scores and label them (1 for genuine, 0 for impostor)
+        scores = [(score, 1) for score in genuine_scores] + [(score, 0) for score in impostor_scores]
+
+        # Sort scores in descending order
+        scores.sort(key=lambda x: x[0], reverse=True)
+
+        # Calculate FAR and TAR for each threshold
+        FARs = []
+        TARs = []
+        total_genuine = len(genuine_scores)
+        total_impostor = len(impostor_scores)
+        TP = 0
+        FP = 0
+
+        for score, label in scores:
+            if label == 1:
+                TP += 1
+            else:
+                FP += 1
+
+            FAR = FP / total_impostor
+            TAR = TP / total_genuine
+
+            FARs.append(FAR)
+            TARs.append(TAR)
+
+        # Find the closest FAR to the target and its corresponding TAR
+        closest_FAR_index = min(range(len(FARs)), key=lambda i: abs(FARs[i] - target_FAR))
+        return FARs[closest_FAR_index], TARs[closest_FAR_index]
+
+    def plot_accuracy_progress(self, similarities, true_labels, index):
+        # Calculate the number of positive and negative samples
+        num_positives = sum(true_labels)
+        num_negatives = len(true_labels) - num_positives
+
+        # Sort the similarities and true labels in descending order
+        sorted_indices = np.argsort(similarities)[::-1]
+        sorted_similarities = np.array(similarities)[sorted_indices]
+        sorted_labels = np.array(true_labels)[sorted_indices]
+
+        # Initialize lists to store accuracy and thresholds
+        accuracy_list = []
+        thresholds_list = []
+
+        # Iterate through thresholds from 0.01 to 0.99 by step of 0.01
+        for threshold in np.arange(0.01, 1, 0.01):
+            # Compute predictions based on the current threshold
+            predictions = (sorted_similarities >= threshold).astype(int)
+
+            # Calculate accuracy
+            accuracy = np.sum(predictions == sorted_labels) / len(predictions)
+
+            # Append accuracy and threshold to the lists
+            accuracy_list.append(accuracy)
+            thresholds_list.append(threshold)
+
+        # Plot accuracy progress
+        plt.plot(thresholds_list, accuracy_list)
+        plt.xlabel('Threshold')
+        plt.ylabel('Accuracy')
+        plt.title('Accuracy Progress')
+        plt.grid(True)
+        plt.savefig('accuracy_progress-' + str(index) + '.png')
+        plt.close()
+
+    def calculate_far(self, similarities, labels, threshold):
+        # Calculate the number of false acceptances
+        false_acceptances = np.sum((similarities >= threshold) & (labels == 0))
+        # Calculate the total number of impostor trials
+        total_impostor_trials = np.sum(labels == 0)  # Count the number of impostor instances
+        # Calculate the False Acceptance Rate (FAR)
+        far = false_acceptances / total_impostor_trials
+        return far
+
+    def find_threshold_for_far(self, similarities, labels, target_far):
+        # Sort similarities in descending order
+        sorted_similarities = np.sort(similarities)[::-1]
+        # Iterate through sorted similarities and find the threshold corresponding to target FAR
+        for similarity in sorted_similarities:
+            far = self.calculate_far(similarities, labels, similarity)
+            if far <= target_far:
+                return similarity
+        # If no threshold found for the target FAR, return None
+        return None
+
+    def plot_tar_progress(self, similarities, true_labels, index):
+        # Calculate the number of positive and negative samples
+        num_positives = sum(true_labels)
+        num_negatives = len(true_labels) - num_positives
+
+        # Sort the similarities and true labels in descending order
+        sorted_indices = np.argsort(similarities)[::-1]
+        sorted_similarities = np.array(similarities)[sorted_indices]
+        sorted_labels = np.array(true_labels)[sorted_indices]
+
+        # Initialize lists to store TAR scores for each threshold
+        tar_scores = []
+
+        # Iterate through thresholds from 0.01 to 0.99 by step of 0.01
+        thresholds = np.arange(0.01, 1, 0.01)
+        for threshold in thresholds:
+            # Compute predictions based on the current threshold
+            predictions = (sorted_similarities >= threshold).astype(int)
+
+            # Calculate True Acceptance Rate (TAR)
+            tar = np.sum((predictions == 1) & (sorted_labels == 1)) / num_positives
+
+            # Append TAR to the list
+            tar_scores.append(tar)
+
+        # Plot TAR progress
+        plt.plot(thresholds, tar_scores)
+        plt.xlabel('Threshold')
+        plt.ylabel('True Acceptance Rate (TAR)')
+        plt.title('TAR Progress with Threshold Variation')
+        plt.grid(True)
+        plt.savefig('tar_progress-' + str(index) + '.png')
+        plt.close()
+
+    def _print_all_tar_at_far(self, fpr, tpr):
+        for i in range(2, 7):
+            self._print_tar_at_far(fpr, tpr, 10 ** (-i))
+
     @staticmethod
     def _get_best_threshold(fpr, tpr, thresholds, max_t):
+        # indexes = [True if rate <= 0.001 else False for rate in fpr]
         # indexes = [True if rate <= max_t else False for rate in fpr]
         # best_index = numpy.argmax(tpr[indexes])
         # return thresholds[indexes][best_index]
         return thresholds[np.nanargmin(np.abs(fpr - (1 - tpr)))]
 
     @staticmethod
-    def _save_roc(fpr, tpr, roc_auc, path):
-        plt.figure(figsize=(12, 6))
-        plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = {:.2f})'.format(roc_auc))
-        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.title('Receiver Operating Characteristic Curve')
-        plt.legend(loc='lower right')
+    def _save_roc(fpr, tpr, thresholds, roc_auc, path):
+        eer_threshold = thresholds[np.argmin(np.abs(fpr - (1 - tpr)))]
+        print("EER Threshold: ", eer_threshold)
+        print("EER Accuracy: ", 1 - (fpr[np.nanargmin(np.abs(fpr - (1 - tpr)))] + (1 - tpr[np.nanargmin(np.abs(fpr - (1 - tpr)))])) / 2)
+
+        # Calculate FAR and FRR
+        far = fpr
+        frr = 1 - tpr
+        accuracy = [1 - (ar + rr) / 2 for ar, rr in zip(far, frr)]
+
+        # Plot FAR and FRR curves
+        plt.plot(thresholds, far, label='FAR (False Acceptance Rate)')
+        plt.plot(thresholds, frr, label='FRR (False Rejection Rate)')
+        plt.plot(thresholds, accuracy, label='Accuracy')
+        plt.xlabel('Threshold')
+        plt.ylabel('Rate')
+        plt.yscale('log')
+        plt.ylim([1e-10, 1])
+        plt.legend()
+        plt.title('FAR and FRR curves for EER')
+        plt.grid(True)
         plt.savefig(path)
+        plt.close()
+
+    def _save_tpr_fpr(self, tpr, fpr, path):
+        plt.plot(tpr, fpr)
+        plt.xlabel('TPR')
+        plt.ylabel('FPR')
+        plt.yscale('log')
+        plt.grid(True)
+        plt.savefig(path)
+        plt.close()
 
     def _threshold_based_statistics(self, threshold):
         stats = {"true_positive": 0, "false_positive": 0, "false_negative": 0, "true_negative": 0}
