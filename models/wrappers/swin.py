620bbe51315b7da58324c2c26f4c82761eee5e25
Šimon Strýček <xstryc06@stud.fit.vutbr.cz>
2024-04-08 12:13:00 +0200

    Change SWIN architecture for smaller one

--- models/wrappers/swin.py
+++ models/wrappers/swin.py
@@ -1,12 +1,12 @@
 from torch.nn import Module
 from models.head import EmbeddingHead
-from torchvision.models import swin_b
+from torchvision.models import swin_t
 
 
 class SWINWrapper(Module):
     def __init__(self, embedding_size, num_classes):
         super(SWINWrapper, self).__init__()
-        self.backbone = swin_b()
+        self.backbone = swin_t(pretrained=True)
         self.backbone.head = EmbeddingHead(self.backbone.head.in_features, embedding_size, num_classes)
 
     def forward(self, x):
