b45c46ade817252829359d426e8286b1f3a7dc13
Šimon Strýček <xstryc06@stud.fit.vutbr.cz>
2024-04-20 13:36:12 +0200

    Fix graphs

--- verification/metrics.py
+++ verification/metrics.py
@@ -1,3 +1,6 @@
+import gc
+import glob
+
 import numpy
 import torch
 from sklearn.metrics import roc_curve, auc
@@ -7,7 +10,16 @@ from sklearn.metrics import f1_score, accuracy_score
 from eer import eer
 import numpy as np
 import bisect
+import random
 
+from models.lightning_wrapper import LightningWrapper
+
+DETAILED = False
+#SEED = 69
+SEED = 69
+torch.manual_seed(SEED)
+np.random.seed(SEED)
+random.seed(SEED)
 
 def _generate_roc(similarities, labels):
     fpr, tpr, thresholds = roc_curve(labels, similarities, drop_intermediate=False)
@@ -57,7 +69,7 @@ def _f1_accuracy_eer(similarities, labels, best_threshold):
     return f1, accuracy, eer_score
 
 
-def detailed_roc_curve(labels, scores, num_thresholds=10000):
+def _detailed_roc_curve(labels, scores, num_thresholds=10000):
     thresholds = np.linspace(min(scores), max(scores), num_thresholds)
     tpr = []
     fpr = []
@@ -85,48 +97,130 @@ def detailed_roc_curve(labels, scores, num_thresholds=10000):
     return np.array(fpr), np.array(tpr), thresholds
 
 
+def test_dir(dir, config, model, number_of_classes, eval_dataloader, output_dir):
+    search_pattern = os.path.join(dir, "**", '*.pth' if config.old_checkpoint_format else '*.ckpt')
+    device = torch.device("cuda:" + str(config.devices[0]))
+    f1s, accuracys, max_accuracys, eer_scores, thresholds = [], [], [], [], []
+
+    for idx, ckpt_file in enumerate(sorted(glob.glob(search_pattern, recursive=True))):
+        print("----------------------------")
+        print("Checking file: ", ckpt_file)
+
+        if config.old_checkpoint_format:
+            model.load_backbone_weights(ckpt_file)
+            lightning_model = LightningWrapper(
+                model=model,
+                config=config,
+                num_classes=number_of_classes
+            )
+        else:
+            lightning_model = LightningWrapper.load_from_checkpoint(
+                ckpt_file, model=model,
+                config=config,
+                num_classes=number_of_classes,
+                map_location=device,
+                strict=False
+            )
+
+        lightning_model.to(device)
+        f1, accuracy, max_accuracy, eer_score, threshold = test_and_print(lightning_model, eval_dataloader, device, output_dir, idx)
+        f1s.append(f1)
+        accuracys.append(accuracy)
+        max_accuracys.append(max_accuracy)
+        eer_scores.append(eer_score)
+        thresholds.append(threshold)
+
+        del lightning_model
+        gc.collect()
+        torch.cuda.empty_cache()
+
+    _plot_stats(f1s, max_accuracys, eer_scores, thresholds, output_dir)
+    return f1s, accuracys, max_accuracys, eer_scores, thresholds
+
 def test_and_print(model, dataloader, device, output_dir, index):
     similarities, labels = _run_stats(model, dataloader, device)
     fpr, tpr, thresholds, roc_auc = _generate_roc(similarities, labels)
     best_threshold = _get_best_threshold(fpr, tpr, thresholds)
+    f1, accuracy, eer_score = _f1_accuracy_eer(similarities, labels, best_threshold)
 
     _print_all_tar_at_far(fpr, tpr)
-    _print_stats(similarities, labels, best_threshold)
+    _print_stats(f1, accuracy, eer_score, best_threshold)
 
-    _plot_far_frr(fpr, tpr, thresholds, best_threshold, os.path.join(output_dir, "far_frr-" + str(index) + ".png"))
-    fpr, tpr, thresholds = detailed_roc_curve(labels, similarities)
-    _plot_tpr_fpr(tpr, fpr, os.path.join(output_dir, "tpr_fpr-" + str(index) + ".png"))
-    _plot_accuracy_progress(similarities, labels, best_threshold, os.path.join(output_dir, "accuracy-" + str(index) + ".png"))
+    _plot_far_frr(fpr, tpr, thresholds, eer_score, os.path.join(output_dir, "far_frr-" + str(index) + ".png"))
+    if DETAILED:
+        fpr, tpr, thresholds = _detailed_roc_curve(labels, similarities)
+    _plot_roc(tpr, fpr, os.path.join(output_dir, "roc-" + str(index) + ".png"))
+    max_accuracy = _plot_accuracy_progress(similarities, labels, os.path.join(output_dir, "accuracy-" + str(index) + ".png"))
+
+    return f1, accuracy, max_accuracy, eer_score, best_threshold
 
 
-def _plot_accuracy_progress(similarities, labels, best_threshold, path):
+def _plot_stats(f1, max_accuracy, eer_score, threshold, path):
+    plt.title("F1 score")
+    plt.plot(f1)
+    plt.xlabel('Epoch')
+    plt.ylabel('Score')
+    plt.ylim([0.5, 1])
+    plt.savefig(os.path.join(path, "f1.png"))
+    plt.close()
+
+    plt.title("Accuracy")
+    plt.plot(max_accuracy)
+    plt.axhline(y=max(max_accuracy), label='Maximal accuracy (' + '{:.3f}'.format(max(max_accuracy)) + ')',
+                linestyle=':', color='red')
+    plt.xlabel('Epoch')
+    plt.ylabel('Score')
+    plt.ylim([0.5, 1])
+    plt.legend()
+    plt.savefig(os.path.join(path, "acc.png"))
+    plt.close()
+
+    plt.title("Equal Error Rate")
+    plt.plot(eer_score)
+    plt.xlabel('Epoch')
+    plt.ylabel('EER')
+    plt.ylim([0, 0.4])
+    plt.savefig(os.path.join(path, "eer.png"))
+    plt.close()
+
+    plt.title("EER threshold")
+    plt.plot(threshold)
+    plt.xlabel('Epoch')
+    plt.ylabel('Threshold')
+    # plt.ylim([0, 1])
+    plt.savefig(os.path.join(path, "threshold.png"))
+    plt.close()
+
+
+def _plot_accuracy_progress(similarities, labels, path):
+    num_thresholds = 1000 if DETAILED else 100
     accuracies = []
-    thresholds = [i / 1000 for i in range(1, 1001)]
+    thresholds = [i / num_thresholds for i in range(1, num_thresholds + 1)]
 
     for threshold in thresholds:
         preds = _preds(similarities, threshold)
         accuracies.append(accuracy_score(labels, preds))
 
-    print("MAX ACCURACY:", max(accuracies), "at threshold:", thresholds[np.argmax(accuracies)])
+    max_acc = max(accuracies)
+    print("MAX ACCURACY:", max_acc, "at threshold:", thresholds[np.argmax(accuracies)])
+    plt.title("Accuracy/Threshold curve")
     plt.plot(thresholds, accuracies, label='Accuracy')
     plt.xlabel('Threshold')
     plt.ylabel('Accuracy')
-    plt.axvline(x=best_threshold, label='Optimal threshold value', color='red')
+    plt.axhline(y=max_acc, label='Maximal accuracy (' + '{:.3f}'.format(max_acc) + ')',
+                linestyle=':', color='red')
     plt.legend()
-    plt.ylim([0, 1])
+    plt.ylim([0.5, 1])
     plt.xlim([0, 1])
     plt.grid(True)
     plt.savefig(path)
     plt.close()
+    return max_acc
 
-def _plot_tpr_fpr(tpr, fpr, path):
+
+def _plot_roc(tpr, fpr, path):
+    plt.title("ROC curve")
     plt.plot(fpr, tpr)
-    # plt.axvline(x=1e-2, color="red", label="Testing threshold")
-    # plt.axvline(x=1e-3, color="red", label="Testing threshold")
-    # plt.axvline(x=1e-4, color="red", label="Testing threshold")
-    # plt.axvline(x=1e-5, color="red", label="Testing threshold")
-    # plt.axvline(x=1e-6, color="red", label="Testing threshold")
-    # plt.axvline(x=1e-7, color="red", label="Testing threshold")
     plt.xlabel('FPR (False Positive Rate)')
     plt.ylabel('TPR (True Positive Rate)')
     plt.xscale('log')
@@ -137,13 +231,14 @@ def _plot_tpr_fpr(tpr, fpr, path):
     plt.close()
 
 
-def _plot_far_frr(fpr, tpr, thresholds, best_threshold, path):
+def _plot_far_frr(fpr, tpr, thresholds, eer, path):
     far = fpr
     frr = 1 - tpr
+    eer_threshold = thresholds[np.nanargmin(np.abs(far - frr))]
 
     plt.plot(thresholds, far, label='FAR (False Acceptance Rate)')
     plt.plot(thresholds, frr, label='FRR (False Rejection Rate)')
-    plt.axvline(x=best_threshold, label='Optimal threshold value', color='red')
+    plt.plot(eer_threshold, eer, label='EER (' + '{:.3f}'.format(eer) + ')', color='red', marker='o')
     plt.xlabel('Threshold')
     plt.ylabel('Rate')
     plt.legend()
@@ -155,8 +250,7 @@ def _plot_far_frr(fpr, tpr, thresholds, best_threshold, path):
     plt.close()
 
 
-def _print_stats(similarities, labels, best_threshold):
-    f1, accuracy, eer_score = _f1_accuracy_eer(similarities, labels, best_threshold)
+def _print_stats(f1, accuracy, eer_score, best_threshold):
     print("Optimal F1:", f1)
     print("Optimal Accuracy:", accuracy)
     print("EER:", eer_score)
